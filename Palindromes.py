321200c891ab2ca5740c69e5f986223ac32e2fea
Luis Lizardi <luis.lizardi.12@sansano.usm.cl>
2018-05-10 00:05:10 -0300

    Update Palindromes.py

--- Palindromes.py
+++ Palindromes.py
@@ -1,11 +1,15 @@
 from functions import palindrome,change_basis
 
-n=1000
-print('decimal',' ','smallest base in which the number is a palindrome','\n')
-for number in range(1,n+1):
-    count = 2
-    nume = change_basis(number,count)
-    while not palindrome(nume):
-        count += 1
+def main():
+    n=1000
+    print('decimal',' ','smallest base in which the number is a palindrome','\n')
+    for number in range(1,n+1):
+        count = 2
         nume = change_basis(number,count)
-    print(number,count)
+        while not palindrome(nume):
+            count += 1
+            nume = change_basis(number,count)
+        print(number,count)
+
+if __name__ == "__main__":
+    main()
