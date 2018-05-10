bfecbe12852c61f2546ea4d58fb4baa0cb20069a
Luis Lizardi <luis.lizardi.12@sansano.usm.cl>
2018-05-10 00:32:28 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-def palindrome(number_t):
-    '''return True if *number_t* is a palindrome'''
+def palindrome(number_t=2):
+    #return True if number_t is a palindrome
     if number_t<=0:
         return False
     number_test = str(number_t)
@@ -9,13 +9,11 @@ def palindrome(number_t):
     else:
         return False
 
-def change_basis(number,base):
-    """Return *number* in the new *base*"""
-    l = []
-    while number>0:
-        l.insert(0,str(number%base))
-        number = number//base
-    l = ''.join(l)  # joining to obtain the str(number) 
-    return(int(l))
-
-
+def change_basis(number=2,base=2):
+	#Return *number* in the new base
+	l = []
+	while number>0:
+		l.insert(0,str(number%base))
+		number = number//base
+	l = ''.join(l)  # joining to obtain the str(number) 
+	return(int(l))
